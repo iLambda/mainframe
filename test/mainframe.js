70e4a5f51b5ca5b1d3ee090f0b4bf832837e0be1
Matthieu Freeman <me@ilambda.io>
2016-01-04 04:51:27 +0100

    Updated test/mainframe.js with a valid example.

--- test/mainframe.js
+++ test/mainframe.js
@@ -1,13 +1,56 @@
 // requiring modules
 import Mainframe from '../lib/mainframe.js'
+import keypress from 'keypress'
 
 // create a mainframe
 var mainframe = new Mainframe()
+
+// change the brain model
+mainframe.brain.transitions['IDLE'] = { "keypress": "HI" }
+mainframe.brain.transitions['HI'] = { "ok": "IDLE" }
+mainframe.brain.states['HI'] = function(mainframe, layer, result, args) {
+  // prints 'hi !'
+  mainframe.console.log(`hi ${args.key.name} !`)
+  // go back to idle
+  layer.process('ok')
+}
+// creating a brain layer
+mainframe.brain.bud('main')
+
+// dock a custom console module
 mainframe.modules.dock({
+  name: "console",
   dock: () => {},
   undock: () => {},
-  name: "kek",
-  test: () => console.log('kek')
+
+  log: (d) => console.log("mainframe> " + d)
 })
 
-mainframe.kek.test()
+// add a keydetector agent
+var id = mainframe.agents.register({
+  name: 'keydetector',
+  slots: {
+    register: function(mainframe, args) {
+      // register keypress event
+      keypress(process.stdin)
+        // if key press, propagate to mainframe
+      process.stdin.on('keypress', function (ch, key) {
+        if (key) {
+          // pause the stdin
+          process.stdin.pause()
+          // process the thought
+          mainframe.brain.layer('main').process('keypress', { key: key })
+          // if we don't hit ctrl+c
+          if (!(key.ctrl && key.name === 'c')) {
+            // resume capture
+            process.stdin.resume()
+          }
+        }
+      })
+      // setting raw mode
+      process.stdin.setRawMode(true)
+      // start capture
+      process.stdin.resume()
+    }
+  }
+})
