0975e99a8ffe31606f27712694ca20929a817329
iLambda <me@ilambda.io>
2020-01-30 18:55:00 +0100

    Removed dist folder

--- dist/src/network/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Server = /** @class */ (function () {
-    function Server() {
-    }
-    return Server;
-}());
-exports.Server = Server;
--- dist/src/tools/assert.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var assert_1 = require("assert");
-function assert(condition, msg) {
-    if (!condition) {
-        throw new assert_1.AssertionError({ message: msg });
-    }
-}
-exports.assert = assert;
